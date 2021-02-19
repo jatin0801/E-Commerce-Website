a5e4183dfe0f22f9693ef616cf8a2f0340c05f08
jatin0801 <2018.jatin.chhabria@ves.ac.in>
2021-02-19 14:20:29 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.components';
+import Header from './components/header/header.component'
+
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Header/>
+      <Switch>
+      <Route exact path='/' component={Homepage} />
+      <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
